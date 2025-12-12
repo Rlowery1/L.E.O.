a0331c049909dcae9cd166c892a6c8706f79acc2
Rlowery1 <rllax777@gmail.com>
2025-12-11 23:26:51 -0800

    Commit 57

--- Plugins/AAAtrafficSystem/Intermediate/Build/Win64/UnrealEditor/Inc/TrafficRuntime/UHT/TrafficRuntime.init.gen.cpp
+++ Plugins/AAAtrafficSystem/Intermediate/Build/Win64/UnrealEditor/Inc/TrafficRuntime/UHT/TrafficRuntime.init.gen.cpp
@@ -17,13 +17,13 @@ static_assert(!UE_WITH_CONSTINIT_UOBJECT, "This generated code can only be compi
 			nullptr,
 			0,
 			PKG_CompiledIn | 0x00000000,
-			0x83D6D7B9,
-			0x6F3E985A,
+			0xFAFB5F55,
+			0x6E45CA28,
 			METADATA_PARAMS(0, nullptr)
 		};
 		UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_TrafficRuntime.OuterSingleton, PackageParams);
 	}
 	return Z_Registration_Info_UPackage__Script_TrafficRuntime.OuterSingleton;
 }
-static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_TrafficRuntime(Z_Construct_UPackage__Script_TrafficRuntime, TEXT("/Script/TrafficRuntime"), Z_Registration_Info_UPackage__Script_TrafficRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x83D6D7B9, 0x6F3E985A));
+static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_TrafficRuntime(Z_Construct_UPackage__Script_TrafficRuntime, TEXT("/Script/TrafficRuntime"), Z_Registration_Info_UPackage__Script_TrafficRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xFAFB5F55, 0x6E45CA28));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Plugins/AAAtrafficSystem/Source/TrafficRuntime/Private/TrafficGeometryProviderFactory.cpp
+++ Plugins/AAAtrafficSystem/Source/TrafficRuntime/Private/TrafficGeometryProviderFactory.cpp
@@ -3,6 +3,8 @@
 #include "CityBLDRoadGeometryProvider.h"
 #include "StaticMeshRoadGeometryProvider.h"
 #include "TrafficRuntimeModule.h"
+#include "TrafficRuntimeSettings.h"
+#include "UObject/UObjectIterator.h"
 #include "Interfaces/IPluginManager.h"
 
 TObjectPtr<UObject> UTrafficGeometryProviderFactory::CreateProvider(
@@ -12,25 +14,27 @@ TObjectPtr<UObject> UTrafficGeometryProviderFactory::CreateProvider(
 	OutInterface = nullptr;
 	UObject* ProviderObject = nullptr;
 
-	const TSharedPtr<IPlugin> CityBLDPlugin = IPluginManager::Get().FindPlugin(TEXT("CityBLD"));
-	const bool bUseCityBLD = CityBLDPlugin.IsValid() && CityBLDPlugin->IsEnabled();
+	if (GetDefault<UTrafficRuntimeSettings>()->bEnableCityBLDAdapter)
+	{
+		for (TObjectIterator<UClass> It; It; ++It)
+		{
+			if (It->GetName().Contains(TEXT("BP_MeshRoad")))
+			{
+				UE_LOG(LogTraffic, Log, TEXT("[GeometryProviderFactory] Found BP_MeshRoad class; using CityBLD provider."));
+				UCityBLDRoadGeometryProvider* CityProvider =
+					NewObject<UCityBLDRoadGeometryProvider>(GetTransientPackage());
+				OutInterface = static_cast<ITrafficRoadGeometryProvider*>(CityProvider);
+				ProviderObject = CityProvider;
+				return ProviderObject;
+			}
+		}
+	}
 
-	if (bUseCityBLD)
-	{
-		UE_LOG(LogTraffic, Log, TEXT("[GeometryProviderFactory] Using CityBLD geometry provider."));
-		UCityBLDRoadGeometryProvider* CityProvider =
-			NewObject<UCityBLDRoadGeometryProvider>(GetTransientPackage());
-		OutInterface = static_cast<ITrafficRoadGeometryProvider*>(CityProvider);
-		ProviderObject = CityProvider;
-	}
-	else
-	{
-		UE_LOG(LogTraffic, Log, TEXT("[GeometryProviderFactory] Using static mesh provider."));
-		UStaticMeshRoadGeometryProvider* MeshProvider =
-			NewObject<UStaticMeshRoadGeometryProvider>(GetTransientPackage());
-		OutInterface = static_cast<ITrafficRoadGeometryProvider*>(MeshProvider);
-		ProviderObject = MeshProvider;
-	}
+	UE_LOG(LogTraffic, Log, TEXT("[GeometryProviderFactory] Using static mesh provider."));
+	UStaticMeshRoadGeometryProvider* MeshProvider =
+		NewObject<UStaticMeshRoadGeometryProvider>(GetTransientPackage());
+	OutInterface = static_cast<ITrafficRoadGeometryProvider*>(MeshProvider);
+	ProviderObject = MeshProvider;
 
 	return ProviderObject;
 }
